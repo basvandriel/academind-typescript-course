16e17c0829b84136d069505babe8bac602a21241
Bas van Driel <contact@basvandriel.nl>
2020-12-06 15:02:25 +0100

    add function arguments with type checking

--- using-ts-module/app.ts
+++ using-ts-module/app.ts
@@ -1,15 +1,21 @@
 console.log("Starting up!");
 
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, printResult: boolean, phrase: string) {
   if (typeof n1 !== "number" || typeof n2 !== "number") {
     throw new Error("Incorrect input!");
   }
-  return n1 + n2;
+
+  const result = n1 + n2;
+
+  return printResult ? console.log(phrase + result) : result;
 }
 
 const number1 = 5;
 const number2 = 2.8;
 
-const result = add(number1, number2);
+const printResult = true;
+const phrase = "Result is : ";
+
+const result = add(number1, number2, printResult, phrase);
 
 console.log(result);
