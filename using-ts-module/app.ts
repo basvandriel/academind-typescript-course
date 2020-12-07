81a81dc0c08870baa74b9149923e278bc2afd9f4
Bas van Driel <contact@basvandriel.nl>
2020-12-07 13:59:06 +0100

    type declarations

--- using-ts-module/app.ts
+++ using-ts-module/app.ts
@@ -1,3 +1,17 @@
-const person = { name: "Bas", age: 27 };
+// const person: {
+//   name: string;
+//   age: number;
+// }
+const person = { name: "Bas", age: 27, hobbies: ["Sports", "Cooking"] };
 
-console.log(person.nickname);
+let favoriteActivitites: string[];
+favoriteActivitites = ["Sports"];
+
+let anyArray: any[];
+anyArray = ["Sports", 1, true];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
