f2021d1ee48497d84cad1d0b79ef96275fd8dc8d
Bas van Driel <contact@basvandriel.nl>
2020-12-07 14:16:04 +0100

    Use of enums

--- using-ts-module/app.ts
+++ using-ts-module/app.ts
@@ -2,16 +2,22 @@
 //   name: string;
 //   age: number;
 // }
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Bas",
   age: 27,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 let favoriteActivitites: string[];
@@ -24,4 +30,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("READ ONLY USER");
 }
